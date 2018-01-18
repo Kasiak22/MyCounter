3aa85541616d8ed900bd8a3a19a66a1dbf0869f3
Kate Gardian <kgardian@gmail.com>
2018-01-19 00:52:25 +0100

    Some improvement after mentor's advice

--- app/src/main/java/com/example/android/mycounter/MainActivity.java
+++ app/src/main/java/com/example/android/mycounter/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.android.mycounter;
 
 
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,16 +10,58 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
     int scoreA;
     int scoreB;
     int setA;
     int setB;
+    TextView scoreView_a;
+    TextView scoreView_b;
+    TextView setView_b;
+    TextView setView_a;
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("scoreA", scoreA);
+        outState.putInt("scoreB", scoreB);
+        outState.putInt("setA", setA);
+        outState.putInt("setB", setB);
+
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+       // Warning:(39, 24) Casting 'findViewById(R.id.team_a_score)' to 'TextView' is redundant
+        scoreView_a= findViewById(R.id.team_a_score);
+        scoreView_b = findViewById(R.id.team_b_score);
+        setView_a = findViewById(R.id.team_a_set);
+        setView_b = findViewById(R.id.team_b_set);
+
+
+        if (savedInstanceState != null && savedInstanceState.containsKey("scoreA")) {
+            scoreA = savedInstanceState.getInt("scoreA");
+        }
+        if (savedInstanceState != null && savedInstanceState.containsKey("scoreB")) {
+            scoreB = savedInstanceState.getInt("scoreB");
+        }
+        if (savedInstanceState != null && savedInstanceState.containsKey("setA")) {
+            setA = savedInstanceState.getInt("setA");
+        }
+        if (savedInstanceState != null && savedInstanceState.containsKey("setB")) {
+            setB = savedInstanceState.getInt("setB");
+        }
+
+        displayForTeamA(scoreA);
+        displayForTeamB(scoreB);
+        displaySetTeamA(setA);
+        displaySetTeamB(setB);
     }
+
 
     public void in(View view) {
         scoreA = scoreA + 1;
@@ -30,7 +74,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void let(View view) {
-
         winSet(scoreA);
     }
 
@@ -38,13 +81,12 @@ public class MainActivity extends AppCompatActivity {
      * Displays the given score for Team A.
      */
     public void displayForTeamA(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
-        scoreView.setText(String.valueOf(scoreA));
+
+        scoreView_a.setText(String.valueOf(scoreA));
     }
 
     public void displaySetTeamA(int set) {
-        TextView scoreView = (TextView) findViewById(R.id.team_a_set);
-        scoreView.setText(String.valueOf(setA));
+        setView_a.setText(String.valueOf(setA));
     }
 
     public void inB(View view) {
@@ -62,14 +104,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void displayForTeamB(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
-        scoreView.setText(String.valueOf(scoreB));
+        scoreView_b.setText(String.valueOf(scoreB));
     }
 
     public void displaySetTeamB(int set) {
-        TextView scoreView = (TextView) findViewById(R.id.team_b_set);
-        scoreView.setText(String.valueOf(setB));
-
+        setView_b.setText(String.valueOf(setB));
     }
 
     public void winSet(int score) {
